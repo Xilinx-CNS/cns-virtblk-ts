7a8a6aafd1bf4068f25fed212c1ece5eb97e7107
Denis Pryazhennikov <Denis.Pryazhennikov@arknetworks.am>
2022-09-02 10:12:33 +0100

    ts: use SPDK NVME target instead of linux kernel one
    Ansible scripts from virtblk-ci have changed. SPDK NVME target
    is used now by default. This patch makes all preparations to
    match these changes.
    
    AMD-Jira-Id: VIRTBLK-1797
    Signed-off-by: Denis Pryazhennikov <Denis.Pryazhennikov@arknetworks.am>

--- conf/scripts/backend-helpers/remove_ansible_nvme_server.sh
+++ conf/scripts/backend-helpers/remove_ansible_nvme_server.sh
@@ -2,8 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 # (c) Copyright 2019 - 2022 Xilinx, Inc. All rights reserved.
 
-# Remove NVME server
-nvmetcli clear
+# Remove NVME SPDK server
+systemctl stop nvme-server
+journalctl -u nvme-server
+
+sleep 1
 
 # Unload brd module
 modprobe -r brd
